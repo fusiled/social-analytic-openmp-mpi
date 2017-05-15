f9cbe92f8a3e1aec3392ecc5bf444bb738321bd7
Matteo Fusi <fusiled@gmail.com>
2017-05-15 11:56:25 +0200

    added small comment

--- src/parse_events.h
+++ src/parse_events.h
@@ -4,6 +4,9 @@
 #include "event_list.h"
 #include "top_three.h"
 
+
+//produce a top_three based on the passed events_array. the size of the output array
+//is saved at out_size
 top_three ** parse_events(valued_event* events_array, int size, int* out_size);
 
 #endif
